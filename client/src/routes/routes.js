8771f442ced8e1eb5813d7b48ce8ed6a20367f18
Médy <m.khimoum@gmail.com>
2020-11-11 15:29:13 +0100

    Update Route and files

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -15,24 +15,34 @@ const routes = [
         component: () => import(/* webpackChunkName: "demo" */ '../views/Dashboard.vue')
       },
       {
-        path: '/icons',
-        name: 'icons',
-        component: () => import(/* webpackChunkName: "demo" */ '../views/Icons.vue')
+        path: '/lessons',
+        name: 'lessons',
+        component: () => import(/* webpackChunkName: "demo" */ '../dashboard_component/Lessons.vue')
       },
       {
-        path: '/profile',
-        name: 'profile',
-        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/UserProfile.vue')
+        path: '/users',
+        name: 'Users',
+        component: () => import(/* webpackChunkName: "demo" */ '../dashboard_component/Users.vue')
       },
       {
-        path: '/maps',
-        name: 'maps',
-        component: () => import(/* webpackChunkName: "demo" */ '../views/GoogleMaps.vue')
+        path: '/horses',
+        name: 'Horses',
+        component: () => import(/* webpackChunkName: "demo" */ '../dashboard_component/Horses.vue')
       },
       {
-        path: '/tables',
-        name: 'tables',
-        component: () => import(/* webpackChunkName: "demo" */ '../views/RegularTables.vue')
+        path: '/admins',
+        name: 'Admins',
+        component: () => import(/* webpackChunkName: "demo" */ '../dashboard_component/Admins.vue')
+      },
+      {
+        path: '/horse2rider',
+        name: 'Horse2Rider',
+        component: () => import(/* webpackChunkName: "demo" */ '../dashboard_component/Horse2Rider.vue')
+      },
+      {
+        path: '/lessonsapply',
+        name: 'LessonsApply',
+        component: () => import(/* webpackChunkName: "demo" */ '../dashboard_component/LessonsApply.vue')
       }
     ]
   },
@@ -44,12 +54,30 @@ const routes = [
       {
         path: '/login',
         name: 'login',
-        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/Login.vue')
+        component: () => import(/* webpackChunkName: "demo" */ '../views1/Login.vue')
       },
       {
         path: '/register',
         name: 'register',
-        component: () => import(/* webpackChunkName: "demo" */ '../views/Pages/Register.vue')
+        component: () => import(/* webpackChunkName: "demo" */ '../views1/Register.vue')
+      },
+      {
+        path: '/resetpassword',
+        name: 'ResetPassword',
+        component: () => import(/* webpackChunkName: "demo" */ '../views1/PasswordRecovery.vue')
+      },
+      { path: '*', component: NotFound }
+    ]
+  },
+  {
+    path: '/',
+    redirect: 'profile',
+    component: Profile,
+    children: [
+      {
+        path: '/myprofile',
+        name: 'MyProfile',
+        component: () => import(/* webpackChunkName: "demo" */ '../views1/MyProfile.vue')
       },
       { path: '*', component: NotFound }
     ]
